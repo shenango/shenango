663ea0297c7c9caef9a427f029238542e543f3dc
Josh Fried <joshuafried@gmail.com>
2018-04-09 09:31:31 -0400

    iokernel: add missing file for stats

---
+++ iokernel/stat.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+
+#include <base/log.h>
+
+#include "defs.h"
+
+#define BUFSIZE 4096
+
+uint64_t stats[NR_STATS];
+
+static const char *stat_names[] = {
+	"RX_UNREGISTERED_MAC",
+	"RX_UNICAST_FAIL",
+	"RX_BROADCAST_FAIL",
+	"RX_UNHANDLED",
+	"TX_COMPLETION_OVERFLOW",
+	"TX_COMPLETION_FAIL",
+};
+
+BUILD_ASSERT(ARRAY_SIZE(stat_names) == NR_STATS);
+
+void print_stats(void)
+{
+	int i;
+	char buf[BUFSIZE + 1];
+	size_t done = 0;
+
+	for (i = 0; i < NR_STATS; i++)
+		done += snprintf(buf + done, BUFSIZE - done, "%s: %lu\n", stat_names[i], stats[i]);
+
+	buf[done] = 0;
+
+	log_info("Stats:\n%s", buf);
+}
