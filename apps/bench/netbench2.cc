92251c6b9ccee70a05942ba00605b89607a04bca
Adam Belay <adam@abelay.me>
2019-02-05 04:07:55 +0000

    checkpoint some netbench changes

--- apps/bench/netbench2.cc
+++ apps/bench/netbench2.cc
@@ -1,42 +1,36 @@
 extern "C" {
 #include <base/log.h>
 #include <net/ip.h>
+#include <unistd.h>
 }
 #undef min
 #undef max
 
-#include "thread.h"
-#include "sync.h"
-#include "timer.h"
-#include "net.h"
 #include "fake_worker.h"
+#include "net.h"
+#include "sync.h"
+#include "thread.h"
+#include "timer.h"
 
-#include <iostream>
-#include <iomanip>
-#include <utility>
-#include <memory>
-#include <chrono>
-#include <vector>
 #include <algorithm>
+#include <chrono>
+#include <iomanip>
+#include <iostream>
+#include <memory>
 #include <numeric>
 #include <random>
 #include <utility>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <sstream>
 
 namespace {
-
-constexpr uint64_t kNetbenchPort = 8001;
-
-struct payload {
-  uint64_t work_iterations;
-  uint64_t index;
-};
 
 using namespace std::chrono;
 using sec = duration<double, std::micro>;
 
-// The maximum lateness to tolerate before dropping egress samples.
-constexpr uint64_t kMaxCatchUpUS = 5;
-
+// <- ARGUMENTS FOR EXPERIMENT ->
 // the number of worker threads to spawn.
 int threads;
 // the remote UDP address of the server.
@@ -44,12 +38,94 @@ netaddr raddr;
 // the mean service time in us.
 double st;
 // number of iterations required for 1us on target server
-int iterations_per_us = 83;
-
+constexpr uint64_t kIterationsPerUS = 65; //83
 // Number of seconds to warmup at rate 0
 constexpr uint64_t kWarmupUpSeconds = 5;
 
 static std::vector<std::pair<double, uint64_t>> rates;
+
+
+
+constexpr uint64_t kUptimePort = 8002;
+constexpr uint64_t kUptimeMagic = 0xDEADBEEF;
+struct uptime {
+  uint64_t idle;
+  uint64_t busy;
+};
+
+void UptimeWorker(std::unique_ptr<rt::TcpConn> c) {
+  while (true) {
+    // Receive an uptime request.
+    uint64_t magic;
+    ssize_t ret = c->ReadFull(&magic, sizeof(magic));
+    if (ret != static_cast<ssize_t>(sizeof(magic))) {
+      if (ret == 0 || ret == -ECONNRESET) break;
+      log_err("read failed, ret = %ld", ret);
+      break;
+    }
+
+    // Check for the right magic value.
+    if (ntoh64(magic) != kUptimeMagic) break;
+
+    // Calculate the current uptime.
+    std::ifstream file("/proc/stat");
+    std::string line;
+    std::getline(file, line);
+    std::istringstream ss(line);
+    std::string tmp;
+    uint64_t user, nice, system, idle, iowait, irq, softirq, steal, guest,
+             guest_nice;
+    ss >> tmp >> user >> nice >> system >> idle >> iowait >> irq >> softirq
+       >> steal >> guest >> guest_nice;
+    uptime u = {hton64(idle + iowait),
+                hton64(user + nice + system + irq + softirq + steal)};
+
+    // Send an uptime response.
+    ssize_t sret = c->WriteFull(&u, sizeof(u));
+    if (sret != sizeof(u)) {
+      if (sret == -EPIPE || sret == -ECONNRESET) break;
+      log_err("write failed, ret = %ld", sret);
+      break;
+    }
+  }
+}
+
+void UptimeServer() {
+  std::unique_ptr<rt::TcpQueue> q(
+      rt::TcpQueue::Listen({0, kUptimePort}, 4096));
+  if (q == nullptr) panic("couldn't listen for connections");
+
+  while (true) {
+    rt::TcpConn *c = q->Accept();
+    if (c == nullptr) panic("couldn't accept a connection");
+    rt::Thread([=] { UptimeWorker(std::unique_ptr<rt::TcpConn>(c)); }).Detach();
+  }
+}
+
+uptime ReadUptime() {
+  std::unique_ptr<rt::TcpConn> c(rt::TcpConn::Dial({0, 0},
+                                                   {raddr.ip, kUptimePort}));
+  uint64_t magic = hton64(kUptimeMagic);
+  ssize_t ret = c->WriteFull(&magic, sizeof(magic));
+  if (ret != static_cast<ssize_t>(sizeof(magic)))
+    panic("uptime request failed, ret = %ld", ret);
+  uptime u;
+  ret = c->ReadFull(&u, sizeof(u));
+  if (ret != static_cast<ssize_t>(sizeof(u)))
+    panic("uptime response failed, ret = %ld", ret);
+  return uptime{ntoh64(u.idle), ntoh64(u.busy)};
+}
+
+constexpr uint64_t kNetbenchPort = 8001;
+struct payload {
+  uint64_t work_iterations;
+  uint64_t index;
+  uint64_t tsc_end;
+  uint32_t cpu;
+};
+
+// The maximum lateness to tolerate before dropping egress samples.
+constexpr uint64_t kMaxCatchUpUS = 5;
 
 void ServerWorker(std::unique_ptr<rt::TcpConn> c) {
   payload p;
@@ -57,44 +133,53 @@ void ServerWorker(std::unique_ptr<rt::TcpConn> c) {
   if (w == nullptr) panic("couldn't create worker");
 
   while (true) {
-    // Receive a network response.
+    // Receive a work request.
     ssize_t ret = c->ReadFull(&p, sizeof(p));
-    if (ret <= 0 || ret > static_cast<ssize_t>(sizeof(p))) {
+    if (ret != static_cast<ssize_t>(sizeof(p))) {
       if (ret == 0 || ret == -ECONNRESET) break;
-      panic("read failed, ret = %ld", ret);
+      log_err("read failed, ret = %ld", ret);
+      break;
     }
 
     // Perform fake work if requested.
     uint64_t workn = ntoh64(p.work_iterations);
-    if (workn != 0) w->Work(workn); //82.0
+    if (workn != 0) w->Work(workn);
+    p.tsc_end = hton64(rdtscp(&p.cpu));
+    p.cpu = hton32(p.cpu);
 
-    // Send a network request.
+    // Send a work response.
     ssize_t sret = c->WriteFull(&p, ret);
     if (sret != ret) {
       if (sret == -EPIPE || sret == -ECONNRESET) break;
-      panic("write failed, ret = %ld", sret);
+      log_err("write failed, ret = %ld", sret);
+      break;
     }
   }
 }
 
 void ServerHandler(void *arg) {
-  std::unique_ptr<rt::TcpQueue> q(rt::TcpQueue::Listen({0, kNetbenchPort},
-				  4096));
+  rt::Thread([] { UptimeServer(); }).Detach();
+
+  std::unique_ptr<rt::TcpQueue> q(
+      rt::TcpQueue::Listen({0, kNetbenchPort}, 4096));
   if (q == nullptr) panic("couldn't listen for connections");
 
   while (true) {
     rt::TcpConn *c = q->Accept();
     if (c == nullptr) panic("couldn't accept a connection");
-    rt::Thread([=]{ServerWorker(std::unique_ptr<rt::TcpConn>(c));}).Detach();
+    rt::Thread([=] { ServerWorker(std::unique_ptr<rt::TcpConn>(c)); }).Detach();
   }
 }
 
 struct work_unit {
   double start_us, work_us, duration_us;
+  uint64_t tsc;
+  uint32_t cpu;
 };
 
 template <class Arrival, class Service>
-std::vector<work_unit> GenerateWork(Arrival a, Service s, double cur_us, double last_us) {
+std::vector<work_unit> GenerateWork(Arrival a, Service s, double cur_us,
+                                    double last_us) {
   std::vector<work_unit> w;
   while (cur_us < last_us) {
     cur_us += a();
@@ -103,30 +188,32 @@ std::vector<work_unit> GenerateWork(Arrival a, Service s, double cur_us, double 
   return w;
 }
 
-std::vector<work_unit>
-ClientWorker(rt::TcpConn *c, rt::WaitGroup *starter,
-             std::function<std::vector<work_unit>()> wf) {
+std::vector<work_unit> ClientWorker(
+    rt::TcpConn *c, rt::WaitGroup *starter,
+    std::function<std::vector<work_unit>()> wf) {
   constexpr int kBatchSize = 32;
   std::vector<work_unit> w(wf());
-  std::vector<std::chrono::time_point<std::chrono::steady_clock>> timings;
+  std::vector<time_point<steady_clock>> timings;
   timings.reserve(w.size());
 
   // Start the receiver thread.
-  auto th = rt::Thread([&]{
+  auto th = rt::Thread([&] {
     payload rp;
 
     while (true) {
-     ssize_t ret = c->ReadFull(&rp, sizeof(rp));
-     if (ret != static_cast<ssize_t>(sizeof(rp))) {
-       if (ret == 0 || ret < 0) break;
-       panic("read failed, ret = %ld", ret);
-     }
+      ssize_t ret = c->ReadFull(&rp, sizeof(rp));
+      if (ret != static_cast<ssize_t>(sizeof(rp))) {
+        if (ret == 0 || ret < 0) break;
+        panic("read failed, ret = %ld", ret);
+      }
 
-     barrier();
-     auto ts = steady_clock::now();
-     barrier();
-     uint64_t idx = ntoh64(rp.index);
-     w[idx].duration_us = duration_cast<sec>(ts - timings[idx]).count();
+      barrier();
+      auto ts = steady_clock::now();
+      barrier();
+      uint64_t idx = ntoh64(rp.index);
+      w[idx].duration_us = duration_cast<sec>(ts - timings[idx]).count();
+      w[idx].tsc = ntoh64(rp.tsc_end);
+      w[idx].cpu = ntoh32(rp.cpu);
     }
   });
 
@@ -135,7 +222,7 @@ ClientWorker(rt::TcpConn *c, rt::WaitGroup *starter,
   starter->Wait();
 
   barrier();
-  auto expstart = std::chrono::steady_clock::now();
+  auto expstart = steady_clock::now();
   barrier();
 
   payload p[kBatchSize];
@@ -163,7 +250,7 @@ ClientWorker(rt::TcpConn *c, rt::WaitGroup *starter,
     barrier();
 
     // Enqueue a network request.
-    p[j].work_iterations = hton64(static_cast<uint64_t>(w[i].work_us * iterations_per_us));
+    p[j].work_iterations = hton64(w[i].work_us * kIterationsPerUS);
     p[j].index = hton64(i);
     j++;
 
@@ -175,17 +262,16 @@ ClientWorker(rt::TcpConn *c, rt::WaitGroup *starter,
     }
   }
 
-  rt::Sleep(5000 * rt::kMilliseconds);
-
+  //rt::Sleep(1 * rt::kSeconds);
   c->Shutdown(SHUT_RDWR);
   th.Join();
 
   return w;
 }
 
-std::vector<work_unit>
-RunExperiment(int threads, double *reqs_per_sec,
-              std::function<std::vector<work_unit>()> wf) {
+std::vector<work_unit> RunExperiment(
+    int threads, double *reqs_per_sec, double *cpu_usage,
+    std::function<std::vector<work_unit>()> wf) {
   // Create one TCP connection per thread.
   std::vector<std::unique_ptr<rt::TcpConn>> conns;
   for (int i = 0; i < threads; ++i) {
@@ -199,7 +285,7 @@ RunExperiment(int threads, double *reqs_per_sec,
   std::vector<rt::Thread> th;
   std::unique_ptr<std::vector<work_unit>> samples[threads];
   for (int i = 0; i < threads; ++i) {
-    th.emplace_back(rt::Thread([&, i]{
+    th.emplace_back(rt::Thread([&, i] {
       auto v = ClientWorker(conns[i].get(), &starter, wf);
       samples[i].reset(new std::vector<work_unit>(std::move(v)));
     }));
@@ -211,130 +297,141 @@ RunExperiment(int threads, double *reqs_per_sec,
 
   // |--- start experiment duration timing ---|
   barrier();
-  auto start = std::chrono::steady_clock::now();
+  auto start = steady_clock::now();
   barrier();
+  uptime u1 = ReadUptime();
 
   // Wait for the workers to finish.
-  for (auto& t: th)
-    t.Join();
+  for (auto &t : th) t.Join();
 
   // |--- end experiment duration timing ---|
   barrier();
-  auto finish = std::chrono::steady_clock::now();
+  auto finish = steady_clock::now();
   barrier();
+  uptime u2 = ReadUptime();
 
   // Close the connections.
-  for (auto& c: conns)
-    c->Abort();
+  for (auto &c : conns) c->Abort();
 
   // Aggregate all the samples together.
-  uint64_t total = 0;
   std::vector<work_unit> w;
   for (int i = 0; i < threads; ++i) {
     auto &v = *samples[i];
-    total += v.size();
     w.insert(w.end(), v.begin(), v.end());
   }
 
+  // Remove requests that did not complete.
+  w.erase(std::remove_if(w.begin(), w.end(),
+                         [](const work_unit &s) { return s.duration_us == 0; }),
+          w.end());
+
   // Report results.
-  double elapsed = std::chrono::duration_cast<sec>(finish - start).count();
+  double elapsed = duration_cast<sec>(finish - start).count();
   if (reqs_per_sec != nullptr)
-    *reqs_per_sec = static_cast<double>(total) / elapsed * 1000000;
+    *reqs_per_sec = static_cast<double>(w.size()) / elapsed * 1000000;
+  uint64_t idle = u2.idle - u1.idle;
+  uint64_t busy = u2.busy - u1.busy;
+  if (cpu_usage != nullptr)
+    *cpu_usage = static_cast<double>(busy) /
+                 static_cast<double>(idle + busy);
   return w;
 }
 
-void SteadyStateExperiment(int threads, double req_rate, double service_time) {
-  constexpr int kRounds = 1;
-  std::vector<work_unit> w;
-  double reqs_per_sec = 0;
-
-  for (int i = 0; i < kRounds; i++) {
-    double tmp;
-    auto t = RunExperiment(threads, &tmp, [=]{
-      std::mt19937 g(rand());
-      std::exponential_distribution<double>
-        rd(1.0 / (1000000.0 / (req_rate / static_cast<double>(threads))));
-      std::exponential_distribution<double> wd(1.0 / service_time);
-      return GenerateWork(std::bind(rd, g), std::bind(wd, g), 0, 1000000); 
-    });
-    w.insert(w.end(), t.begin(), t.end());
-    reqs_per_sec += tmp;
-    rt::Sleep(500 * rt::kMilliseconds);
+void PrintRawResults(std::vector<work_unit> w) {
+  std::sort(w.begin(), w.end(), [](const work_unit &s1, work_unit &s2) {
+    return s1.tsc < s2.tsc;
+  });
+  for (const work_unit& u : w) {
+    std::cout << std::setprecision(2) << std::fixed
+              << u.start_us << "," << u.duration_us << "," << u.work_us << ","
+              << u.tsc << "," << u.cpu << std::endl;
   }
+}
 
-  w.erase(std::remove_if(w.begin(), w.end(),
-                         [](const work_unit& s){return s.duration_us == 0;}),
-          w.end());
-  reqs_per_sec /= kRounds;
-  std::sort(w.begin(), w.end(),
-            [](const work_unit& s1, work_unit& s2){
-              return s1.duration_us < s2.duration_us;});
-  double sum = std::accumulate(w.begin(), w.end(), 0.0,
-    [](double s, const work_unit& c){return s + c.duration_us;});
+void PrintStatResults(std::vector<work_unit> w,
+                      double offered_rps, double rps, double cpu_usage) {
+  std::sort(w.begin(), w.end(), [](const work_unit &s1, work_unit &s2) {
+    return s1.duration_us < s2.duration_us;
+  });
+  double sum = std::accumulate(
+      w.begin(), w.end(), 0.0,
+      [](double s, const work_unit &c) { return s + c.duration_us; });
   double mean = sum / w.size();
   double count = static_cast<double>(w.size());
-  double p9 = w[count * 0.9].duration_us;
+  double p90 = w[count * 0.9].duration_us;
   double p99 = w[count * 0.99].duration_us;
   double p999 = w[count * 0.999].duration_us;
   double p9999 = w[count * 0.9999].duration_us;
   double min = w[0].duration_us;
   double max = w[w.size() - 1].duration_us;
-  std::cout << std::setprecision(2) << std::fixed
-            << "t: "       << threads
-            << " rps: "    << reqs_per_sec
-            << " n: "      << w.size()
-            << " min: "    << min
-            << " mean: "   << mean
-            << " 90%: "    << p9
-            << " 99%: "    << p99
-            << " 99.9%: "  << p999
-            << " 99.99%: " << p9999
-            << " max: "    << max << std::endl;
+  std::cout //<< "#threads,offered_rps,rps,cpu_usage,samples,min,mean,p90,p99,p999,p9999,max"
+            //<< std::endl
+            << std::setprecision(4) << std::fixed
+            << threads << ","
+            << offered_rps << ","
+            << rps << ","
+            << cpu_usage << ","
+            << w.size() << ","
+            << min << ","
+            << mean << ","
+            << p90 << ","
+            << p99 << ","
+            << p999 << ","
+            << p9999 << ","
+            << max << std::endl;
 }
 
-void LoadShiftExperiment(int threads, const std::vector<std::pair<double, uint64_t>> &rates,
+void SteadyStateExperiment(int threads, double offered_rps,
+                           double service_time) {
+  double rps, cpu_usage;
+  std::vector<work_unit> w = RunExperiment(threads, &rps, &cpu_usage, [=] {
+    std::mt19937 rg(rand());
+    std::mt19937 dg(rand());
+    std::exponential_distribution<double> rd(
+        1.0 / (1000000.0 / (offered_rps / static_cast<double>(threads))));
+    std::exponential_distribution<double> wd(1.0 / service_time);
+    return GenerateWork(std::bind(rd, rg), std::bind(wd, dg), 0, 20000000);
+  });
+
+  // Print the results.
+  PrintStatResults(w, offered_rps, rps, cpu_usage);
+}
+
+void LoadShiftExperiment(int threads,
+                         const std::vector<std::pair<double, uint64_t>> &rates,
                          double service_time) {
-  auto w = RunExperiment(threads, nullptr, [=]{
-    std::mt19937 g(rand());
+  auto w = RunExperiment(threads, nullptr, nullptr, [=] {
+    std::mt19937 rg(rand());
+    std::mt19937 wg(rand());
     std::exponential_distribution<double> wd(1.0 / service_time);
     std::vector<work_unit> w1;
     uint64_t last_us = 0;
     for (auto &r : rates) {
-      std::exponential_distribution<double>
-        rd(1.0 / (1000000.0 / (r.first / static_cast<double>(threads))));
-      auto work = GenerateWork(std::bind(rd, g), std::bind(wd, g), last_us, last_us + r.second);
+      std::exponential_distribution<double> rd(
+          1.0 / (1000000.0 / (r.first / static_cast<double>(threads))));
+      auto work = GenerateWork(std::bind(rd, rg), std::bind(wd, wg), last_us,
+                               last_us + r.second);
       last_us = work.back().start_us;
-      w1.insert(w1.end(), work.begin(), work.end()); 
+      w1.insert(w1.end(), work.begin(), work.end());
     }
     return w1;
   });
-
-  w.erase(std::remove_if(w.begin(), w.end(),
-                         [](const work_unit& s){return s.duration_us == 0 || s.start_us < kWarmupUpSeconds * 1e6;}),
-          w.end());
-  std::sort(w.begin(), w.end(), [](const work_unit& s1, work_unit& s2){
-    return s1.start_us < s2.start_us;
-  });
-
-  for (const work_unit& i : w) {
-    std::cout << std::setprecision(2) << std::fixed
-              << i.start_us << ":" << i.duration_us << std::endl;
-  }
+  PrintRawResults(w);
 }
 
 void ClientHandler(void *arg) {
   LoadShiftExperiment(threads, rates, st);
 #if 0
-  for (double i = 100000; i <= 8000000; i += 100000)
+  for (double i = 50000; i <= 8000000; i += 50000) {
     SteadyStateExperiment(threads, i, st);
+  }
 #endif
 }
 
 int StringToAddr(const char *str, uint32_t *addr) {
   uint8_t a, b, c, d;
 
-  if(sscanf(str, "%hhu.%hhu.%hhu.%hhu", &a, &b, &c, &d) != 4)
-    return -EINVAL;
+  if (sscanf(str, "%hhu.%hhu.%hhu.%hhu", &a, &b, &c, &d) != 4) return -EINVAL;
 
   *addr = MAKE_IP_ADDR(a, b, c, d);
   return 0;
@@ -351,7 +448,7 @@ std::vector<std::string> split(const std::string &text, char sep) {
   return tokens;
 }
 
-} // anonymous namespace
+}  // anonymous namespace
 
 int main(int argc, char *argv[]) {
   int i, ret;
@@ -374,7 +471,9 @@ int main(int argc, char *argv[]) {
   }
 
   if (argc < 7) {
-    std::cerr << "usage: [cfg_file] client [#threads] [remote_ip] [service_us] [<request_rate>:<us_duration>]..." << std::endl;
+    std::cerr << "usage: [cfg_file] client [#threads] [remote_ip] [service_us] "
+                 "[<request_rate>:<us_duration>]..."
+              << std::endl;
     return -EINVAL;
   }
 
@@ -388,13 +487,14 @@ int main(int argc, char *argv[]) {
 
   for (i = 6; i < argc; i++) {
     std::vector<std::string> tokens = split(argv[i], ':');
-    if (tokens.size() != 2)
-      return -EINVAL;
+    if (tokens.size() != 2) return -EINVAL;
     double rate = std::stod(tokens[0], nullptr);
     uint64_t duration = std::stoll(tokens[1], nullptr, 0);
+#if 0
     if (i == 6) {
-      rates.emplace_back(rate, kWarmupUpSeconds * 1e6); 
+      rates.emplace_back(rate, kWarmupUpSeconds * 1e6);
     }
+#endif
     rates.emplace_back(rate, duration);
   }
 
--- apps/bench/netbench_linux.cc
+++ apps/bench/netbench_linux.cc
@@ -4,13 +4,17 @@ extern "C" {
 #include <netinet/in.h>
 #include <errno.h>
 #include <string.h>
+#include <signal.h>
+
+#include <base/stddef.h>
+#include <base/byteorder.h>
+#include <asm/ops.h>
 }
 #undef min
 #undef max
 
 #include "net.h"
 #include "fake_worker.h"
-#include "proto.h"
 
 #include <iostream>
 #include <iomanip>
@@ -18,40 +22,163 @@ extern "C" {
 #include <memory>
 #include <thread>
 #include <vector>
+#include <fstream>
+#include <string>
+#include <sstream>
 
 namespace {
 
 using sec = std::chrono::duration<double, std::micro>;
+constexpr uint64_t kNetbenchPort = 8001;
+
+// Reads exactly @len bytes from the TCP stream.
+ssize_t ReadFull(int fd, void *buf, size_t len) {
+  char *pos = reinterpret_cast<char*>(buf);
+  size_t n = 0;
+  while (n < len) {
+    ssize_t ret = read(fd, pos + n, len - n);
+    if (ret <= 0) return ret;
+    n += ret;
+  }
+  assert(n == len);
+  return n;
+}
+
+// Writes exactly @len bytes to the TCP stream.
+ssize_t WriteFull(int fd, const void *buf, size_t len) {
+  const char *pos = reinterpret_cast<const char*>(buf);
+  size_t n = 0;
+  while (n < len) {
+    ssize_t ret = write(fd, pos + n, len - n);
+    if (ret < 0) return ret;
+    assert(ret > 0);
+    n += ret;
+  }
+  assert(n == len);
+  return n;
+}
+
+constexpr uint64_t kUptimePort = 8002;
+constexpr uint64_t kUptimeMagic = 0xDEADBEEF;
+struct uptime {
+  uint64_t idle;
+  uint64_t busy;
+};
+
+void UptimeWorker(int fd) {
+  while (true) {
+    // Receive an uptime request.
+    uint64_t magic;
+    ssize_t ret = ReadFull(fd, &magic, sizeof(magic));
+    if (ret != static_cast<ssize_t>(sizeof(magic))) {
+      if (ret == 0 || ret == -ECONNRESET) break;
+      printf("read failed, ret = %ld\n", ret);
+      break;
+    }
+
+    // Check for the right magic value.
+    if (ntoh64(magic) != kUptimeMagic) break;
+
+    // Calculate the current uptime.
+    std::ifstream file("/proc/stat");
+    std::string line;
+    std::getline(file, line);
+    std::istringstream ss(line);
+    std::string tmp;
+    uint64_t user, nice, system, idle, iowait, irq, softirq, steal, guest,
+             guest_nice;
+    ss >> tmp >> user >> nice >> system >> idle >> iowait >> irq >> softirq
+       >> steal >> guest >> guest_nice;
+    uptime u = {hton64(idle + iowait),
+                hton64(user + nice + system + irq + softirq + steal)};
+
+    // Send an uptime response.
+    ssize_t sret = WriteFull(fd, &u, sizeof(u));
+    if (sret != sizeof(u)) {
+      if (sret == -EPIPE || sret == -ECONNRESET) break;
+      printf("write failed, ret = %ld\n", sret);
+      break;
+    }
+  }
+
+  close(fd);
+}
+
+void UptimeServer() {
+  struct sockaddr_in addr;
+  int fd, childfd;
+  int optval;
+
+  if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    printf("socket() failed %d\n", -errno);
+    exit(1);
+  }
+
+  optval = 1;
+  if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR,
+	         (const void *)&optval, sizeof(int)) < 0) {
+    printf("setsockopt() failed %d\n", -errno);
+    exit(1);
+  }
+
+  memset((char *)&addr, 0, sizeof(addr));
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  addr.sin_port = htons(kUptimePort);
+
+  if (bind(fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+    printf("bind() failed %d\n", -errno);
+    exit(1);
+  }
+
+  if (listen(fd, SOMAXCONN) < 0) {
+    printf("listen failed %d\n", -errno);
+    exit(1);
+  }
+
+  while (true) {
+    childfd = accept(fd, NULL, 0);
+    if (childfd == -1) {
+      printf("accept() failed %d\n", -errno);
+      exit(1);
+    }
+
+    std::thread([=] { UptimeWorker(childfd); }).detach();
+  }
+}
 
 void ServerWorker(int fd) {
-  union {
-    unsigned char buf[rt::UdpConn::kMaxPayloadSize];
-    payload p;
-  };
+  struct payload {
+    uint64_t work_iterations;
+    uint64_t index;
+    uint64_t tsc_end;
+    uint32_t cpu;
+  } p;
+
   std::unique_ptr<FakeWorker> w(FakeWorkerFactory("stridedmem:3200:64"));
   if (unlikely(w == nullptr)) exit(1);
 
   while (true) {
     // Receive a network response.
-    ssize_t ret = read(fd, &buf, sizeof(buf));
-    if (ret <= 0 || ret > static_cast<ssize_t>(sizeof(buf))) {
-      if (ret == 0) break;
-      printf("udp read failed, ret = %ld\n", ret);
-      exit(1);
+    ssize_t ret = ReadFull(fd, &p, sizeof(p));
+    if (ret != static_cast<ssize_t>(sizeof(p))) {
+      if (ret == 0 || ret == -ECONNRESET) break;
+      printf("read failed, ret = %ld\n", ret);
+      break;
     }
 
-    // Determine if the connection is being killed.
-    if (unlikely(p.tag == kKill)) break;
-
     // Perform fake work if requested.
-    if (p.workn != 0) w->Work(p.workn * 82.0);
+    uint64_t workn = ntoh64(p.work_iterations);
+    if (workn != 0) w->Work(workn);  // 82.0
+    p.tsc_end = hton64(rdtscp(&p.cpu));
+    p.cpu = hton32(p.cpu);
 
     // Send a network request.
-    ssize_t sret = write(fd, &buf, ret);
+    ssize_t sret = WriteFull(fd, &p, ret);
     if (sret != ret) {
-      if (ret == -EPIPE) break;
-      printf("udp write failed, ret = %ld\n", ret);
-      exit(1);
+      if (ret == -EPIPE || sret == -ECONNRESET) break;
+      printf("write failed, ret = %ld\n", ret);
+      break;
     }
   }
 
@@ -60,10 +187,20 @@ void ServerWorker(int fd) {
 
 void ServerHandler(void *arg) {
   struct sockaddr_in addr;
-  int fd;
+  int fd, childfd;
+  int optval;
 
-  if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+  std::thread([=] { UptimeServer(); }).detach();
+
+  if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
     printf("socket() failed %d\n", -errno);
+    exit(1);
+  }
+
+  optval = 1;
+  if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR,
+	         (const void *)&optval, sizeof(int)) < 0) {
+    printf("setsockopt() failed %d\n", -errno);
     exit(1);
   }
 
@@ -77,78 +214,19 @@ void ServerHandler(void *arg) {
     exit(1);
   }
 
+  if (listen(fd, SOMAXCONN) < 0) {
+    printf("listen failed %d\n", -errno);
+    exit(1);
+  }
+
   while (true) {
-    nbench_req req;
-    sockaddr_in caddr;
-    socklen_t caddr_len = sizeof(caddr);
-    ssize_t ret = recvfrom(fd, &req, sizeof(req), 0, (struct sockaddr *)&caddr,
-                           &caddr_len);
-    if (ret != sizeof(req) || req.magic != kMagic) continue;
+    childfd = accept(fd, NULL, 0);
+    if (childfd == -1) {
+      printf("accept() failed %d\n", -errno);
+      exit(1);
+    }
 
-    std::vector<std::thread> ts;
-
-    auto t = std::thread([=]{
-      printf("got connection %x:%d, %d ports\n",
-             addr.sin_addr.s_addr, addr.sin_port, req.nports);
-
-      union {
-        nbench_resp resp;
-        char buf[rt::UdpConn::kMaxPayloadSize];
-      };
-      resp.magic = kMagic;
-      resp.nports = req.nports;
-
-      // Create the worker threads.
-      std::vector<int> conns;
-      for (int i = 0; i < req.nports; ++i) {
-        int fdin;
-        if ((fdin = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-          printf("socket() failed %d\n", -errno);
-          exit(1);
-        }
-
-        struct sockaddr_in laddr;
-        memset((char *)&laddr, 0, sizeof(laddr));
-        laddr.sin_family = AF_INET;
-        laddr.sin_addr.s_addr = htonl(INADDR_ANY);
-        laddr.sin_port = htons(0);
-
-        if (bind(fdin, (struct sockaddr *)&laddr, sizeof(laddr)) < 0) {
-          printf("bind() failed %d\n", -errno);
-          exit(1);
-        }
-
-        socklen_t addr_len = sizeof(addr);
-        if (getsockname(fdin, (struct sockaddr*)&laddr, &addr_len) < 0) {
-          printf("getsockname() failed %d\n", -errno);
-          exit(1);
-        }
-
-        if (connect(fdin, (struct sockaddr *)&caddr, sizeof(caddr)) < 0) {
-          printf("connect() failed %d\n", -errno);
-          exit(1);
-        }
-
-	resp.ports[i] = ntohs(laddr.sin_port);
-        std::thread(ServerWorker, fdin).detach();
-        conns.emplace_back(fdin);
-      }
-
-      // Send the port numbers to the client.
-      ssize_t len = sizeof(nbench_resp) + sizeof(uint16_t) * req.nports;
-      if (len > static_cast<ssize_t>(rt::UdpConn::kMaxPayloadSize))
-        printf("too big\n");
-      ssize_t ret = sendto(fd, &resp, len, 0, (struct sockaddr *)&caddr,
-                           sizeof(caddr));
-      if (ret != len) {
-        printf("udp write failed, ret = %ld\n", ret);
-      }
-    });
-    ts.emplace_back(std::move(t));
-
-    for (auto& t: ts)
-      t.join();
-    printf("done\n");
+    std::thread([=] { ServerWorker(childfd); }).detach();
   }
 }
 
@@ -159,6 +237,7 @@ int main(int argc, char *argv[]) {
     std::cerr << "usage: [cmd] ..." << std::endl;
     return -EINVAL;
   }
+  signal(SIGPIPE, SIG_IGN);
 
   std::string cmd = argv[1];
   if (cmd.compare("server") == 0) {
