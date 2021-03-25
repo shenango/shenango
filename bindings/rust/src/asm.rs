fbc59784d4e9881bc2b8aa8e25fc434814629b1e
Lucas Bleme <andreybleme1@gmail.com>
2021-03-25 18:54:50 -0300

    Update LLVM-style inline assembly (#4)
    * Update LLVM-style inline assembly
    
    * Replace asm with llvm_asm rust binding

--- bindings/rust/src/asm.rs
+++ bindings/rust/src/asm.rs
@@ -1,16 +1,16 @@
 #[inline]
 pub fn cpu_relax() {
-    unsafe { asm!("pause" :::: "volatile") }
+    unsafe { llvm_asm!("pause" :::: "volatile") }
 }
 #[inline]
 pub fn cpu_serialize() {
-    unsafe { asm!("cpuid" : : : "rax", "rbx", "rcx", "rdx": "volatile") }
+    unsafe { llvm_asm!("cpuid" : : : "rax", "rbx", "rcx", "rdx": "volatile") }
 }
 #[inline]
 pub fn rdtsc() -> u64 {
     let a: u32;
     let d: u32;
-    unsafe { asm!("rdtsc" : "={eax}"(a), "={edx}"(d) : : : "volatile" ) };
+    unsafe { llvm_asm!("rdtsc" : "={eax}"(a), "={edx}"(d) : : : "volatile" ) };
     (a as u64) | ((d as u64) << 32)
 }
 #[inline]
@@ -18,7 +18,7 @@ pub fn rdtscp() -> (u64, u32) {
     let a: u32;
     let d: u32;
     let c: u32;
-    unsafe { asm!("rdtscp" : "={eax}"(a), "={edx}"(d), "={ecx}"(c) : : : "volatile") };
+    unsafe { llvm_asm!("rdtscp" : "={eax}"(a), "={edx}"(d), "={ecx}"(c) : : : "volatile") };
 
     ((a as u64) | ((d as u64) << 32), c)
 }
